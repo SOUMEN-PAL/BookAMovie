ce0623051580cfc811cd3c90311cd741696e51ae
Soumen Pal <soumen@zynetic.co>
2026-08-19 01:56:39 +0530

    first commit

---
+++ src/test/java/org/devbot/bookmymovie/BookMyMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package org.devbot.bookmymovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyMovieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
